a566ef582feec40af62278119f1f796782059520
KBR9 <cabero96@protonmail.com>
2020-10-14 11:30:16 +0200

    lock_file.rs can now register multiple instances running with different configurations

--- src/lock_file.rs
+++ src/lock_file.rs
@@ -1,39 +1,51 @@
 use std::{
     env::temp_dir,
     fs,
-    io::Error,
-    path::PathBuf,
+    io::{
+        Error,
+        prelude::*,
+    },
+    path::{
+        Path,
+        PathBuf,
+    },
 };
-use std::path::Path;
+use std::fs::OpenOptions;
 
 use sysinfo::Pid;
 
-pub struct LockFile<'a> {
+pub struct LockFile {
     path: PathBuf,
-    config: &'a Path,
+    sep: String,
 }
 
-impl<'a> LockFile<'a> {
-    pub fn new(config: &'a Path) -> Self {
+impl LockFile {
+    pub fn new() -> Self {
         LockFile {
             path: temp_dir().join("organizer.lock"),
-            config,
+            sep: "---".to_string(),
         }
     }
 
-    pub fn write(self, pid: Pid) -> Result<(), Error> {
-        fs::write(&self.path, format!("{}\n{}", &pid, self.config.display()))
+    pub fn write(self, pid: Pid, config: &Path) -> Result<(), Error> {
+        fs::write(&self.path, format!("{}\n{}\n{}", pid, config.display(), self.sep))
     }
 
-    fn read(&self) -> Result<Vec<String>, Error> {
+    pub fn append(self, pid: Pid, config: &Path) -> Result<(), Error> {
+        let mut f = OpenOptions::new().append(true).open(config)?;
+        writeln!(f, "{}\n{}\n{}", pid, config.display(), self.sep)
+    }
+
+    pub fn get_sections(&self) -> Result<Vec<(Pid, PathBuf)>, Error> {
         let content = fs::read_to_string(&self.path)?;
-        let lines: Vec<String> = content.lines().map(|x| x.to_string()).collect();
-        Ok(lines)
-    }
-
-    pub fn get_pid_and_config(self) -> Result<(Pid, PathBuf), Error> {
-        let lines = self.read()?;
-        println!("{:?}", lines);
-        Ok((lines.get(0).unwrap().parse::<i32>().unwrap(), PathBuf::from(lines.get(1).unwrap())))
+        let content = content.split(&self.sep);
+        let mut sections = Vec::new();
+        for section in content.into_iter() {
+            let section: Vec<String> = section.lines().map(|x| x.to_string()).collect();
+            let pid = section.get(0).unwrap().parse().unwrap();
+            let path = section.get(1).unwrap().parse().unwrap();
+            sections.push((pid, path))
+        }
+        Ok(sections)
     }
 }
