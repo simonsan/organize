fe2f2898989d745ac26d8bdbe87c43aa43e6180c
KBR9 <cabero96@protonmail.com>
2020-10-14 10:14:25 +0200

    - Writing config path to lockfile to allow multiple instances

--- src/lock_file.rs
+++ src/lock_file.rs
@@ -4,27 +4,36 @@ use std::{
     io::Error,
     path::PathBuf,
 };
+use std::path::Path;
+
 use sysinfo::Pid;
 
-#[derive(Default)]
-pub struct LockFile {
+pub struct LockFile<'a> {
     path: PathBuf,
+    config: &'a Path,
 }
 
-impl LockFile {
-    pub fn new() -> Self {
+impl<'a> LockFile<'a> {
+    pub fn new(config: &'a Path) -> Self {
         LockFile {
             path: temp_dir().join("organizer.lock"),
+            config,
         }
     }
 
-    pub fn write_pid(self, pid: Pid) -> Result<(), Error> {
-        fs::write(&self.path, format!("{}", &pid))
+    pub fn write(self, pid: Pid) -> Result<(), Error> {
+        fs::write(&self.path, format!("{}\n{}", &pid, self.config.display()))
     }
 
-    pub fn read_pid(self) -> Result<Pid, Error> {
+    fn read(&self) -> Result<Vec<String>, Error> {
         let content = fs::read_to_string(&self.path)?;
-        let pid = content.parse::<Pid>().unwrap();
-        Ok(pid)
+        let lines: Vec<String> = content.lines().map(|x| x.to_string()).collect();
+        Ok(lines)
+    }
+
+    pub fn get_pid_and_config(self) -> Result<(Pid, PathBuf), Error> {
+        let lines = self.read()?;
+        println!("{:?}", lines);
+        Ok((lines.get(0).unwrap().parse::<i32>().unwrap(), PathBuf::from(lines.get(1).unwrap())))
     }
 }
