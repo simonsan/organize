b9bf60538a53fbddabc61877a84e82b2b3e96fcf
KBR9 <cabero96@protonmail.com>
2020-10-15 23:48:27 +0200

    changed deserializers visibility and moved path trait to path.rs

---
+++ src/path.rs
@@ -0,0 +1,28 @@
+use std::{
+    env,
+    path::{
+        Path,
+        PathBuf,
+    },
+};
+
+pub trait Expand<T> {
+    fn expand(&self) -> T;
+}
+
+impl Expand<PathBuf> for PathBuf {
+    fn expand(&self) -> PathBuf {
+        self.components()
+            .map(|comp| {
+                let path: &Path = comp.as_ref();
+                let path = path.to_str().unwrap();
+                if path.starts_with('$') {
+                    env::var(path.replace('$', ""))
+                        .unwrap_or_else(|_| panic!("error: environment variable '{}' could not be found", path))
+                } else {
+                    path.to_string()
+                }
+            })
+            .collect()
+    }
+}
--- src/user_config/rules/deserialize.rs
+++ src/user_config/rules/deserialize.rs
@@ -1,3 +1,4 @@
+use crate::path::Expand;
 use core::result::Result::Ok;
 use regex::Regex;
 use serde::{
@@ -5,41 +6,18 @@ use serde::{
     Deserializer,
 };
 use std::{
-    env,
     fs,
-    path::{
-        Path,
-        PathBuf,
-    },
+    path::PathBuf,
 };
 
-pub trait PathExpandable<T> {
-    fn expand(&self) -> T;
-}
-
-impl PathExpandable<PathBuf> for PathBuf {
-    fn expand(&self) -> PathBuf {
-        self.components()
-            .map(|comp| {
-                let path: &Path = comp.as_ref();
-                let path = path.to_str().unwrap();
-                if path.starts_with('$') {
-                    env::var(path.replace('$', ""))
-                        .unwrap_or_else(|_| panic!("error: environment variable '{}' could not be found", path))
-                } else {
-                    path.to_string()
-                }
-            })
-            .collect()
-    }
-}
-
-pub fn deserialize_path<'de, D: Deserializer<'de>>(deserializer: D) -> Result<PathBuf, D::Error> {
+pub(in crate::user_config) fn deserialize_path<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<PathBuf, D::Error> {
     let buf = String::deserialize(deserializer)?;
     if buf.is_empty() {
         panic!("could not parse config file: field 'to' must represent a valid path")
     }
-    let mut path = PathBuf::from(&buf).expand();
+    let path = PathBuf::from(&buf).expand();
     if path.is_relative() {
         panic!("could not parse config file: relative paths not allowed")
     }
@@ -49,13 +27,13 @@ pub fn deserialize_path<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Pa
     Ok(path)
 }
 
-pub fn deserialize_regex<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Regex, D::Error> {
+pub(in crate::user_config) fn deserialize_regex<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Regex, D::Error> {
     let buf = String::deserialize(deserializer)?;
     let regex = Regex::new(&buf).expect("error: could not parse config file (invalid regex)");
     Ok(regex)
 }
 
 #[allow(clippy::trivial_regex)]
-pub fn default_regex() -> Regex {
+pub(in crate::user_config) fn default_regex() -> Regex {
     Regex::new("").unwrap()
 }
